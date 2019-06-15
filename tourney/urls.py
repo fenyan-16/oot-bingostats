21ba98a50bb2c014cd7b6f182923877aea5cb44f
Julia Niemeyer <niemeyer.ju@gmail.com>
2019-06-16 00:31:57 +0200

    added user login/logout and profile page

--- tourney/urls.py
+++ tourney/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('events/', include('events.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile/', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
 ]
