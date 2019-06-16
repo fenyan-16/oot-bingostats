7839c33fa374ffd75171ce824d1ee6d45c30a6fa
Julia Niemeyer <niemeyer.ju@gmail.com>
2019-06-16 13:01:04 +0200

    created eventlist for users

--- events/urls.py
+++ events/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: /events/5/
     path('<int:event_id>/', views.detail, name='detail'),
+    path('myevents/', views.EventsByUserListView.as_view(), name='my-events'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, ListView
+from .models import Event
 
 
 def index(request):
@@ -6,3 +9,12 @@ def index(request):
 
 def detail(request, event_id):
     return HttpResponse("You're looking at question %s." % event_id)
+
+
+class EventsByUserListView(LoginRequiredMixin, ListView):
+    model = Event
+    template_name = 'accounts/myevents.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Event.objects.filter(owner=self.request.user).order_by('name')
