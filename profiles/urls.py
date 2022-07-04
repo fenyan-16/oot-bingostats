4a14daafcc79681923f2df21dc5c5d97a1effcf8
Maximilian Weitzel <maximilian.weitzel@ambiotex.com>
2022-07-04 14:47:43 +0200

    Fixing deprecated django method url -> re_path

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 urlpatterns = [
     path('<int:user_id>/edit', views.profile_edit, name='profile-edit'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('<int:user_id>/', views.profile_detail, name='profile-datail'),
     path('<slug:user_name>/', views.profile_detail_name, name='profile-datail_name'),
-    url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    re_path(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
 ]
