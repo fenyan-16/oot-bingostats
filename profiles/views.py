9779a645c7164e56c169b15d7419ced8c0c360c5
Maximilian Weitzel <maximilian.weitzel@ambiotex.com>
2022-07-04 14:45:28 +0200

    Fixing deprecated django method

--- profiles/views.py
+++ profiles/views.py
@@ -19,7 +19,7 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 import datetime
 from django.contrib.auth.tokens import default_token_generator
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .services import AccountActivationTokenGenerator, get_my_latest_results, update_profile
@@ -50,7 +50,7 @@ def signup(request):
 
 def activate(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
@@ -127,7 +127,7 @@ def profile_detail_name(request, user_name):
 
 def activate_account(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
